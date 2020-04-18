1a16d7058b4dde9bd33c7cd5875f01fd4c9a2580
Takec-a <atcoder1981se@gmail.com>
2020-04-19 03:19:57 +0900

    intit

--- app/controllers/linebot_controller.rb
+++ app/controllers/linebot_controller.rb
@@ -22,12 +22,20 @@ class LinebotController < ApplicationController
     events = client.parse_events_from(body)
 
     events.each { |event|
-    if event.message['text'].include?("type a")
-        response = "changed"
-    elseif event.message['text'].include?("1")
-        response = "abc"
-    else response = "ok"
-    end
+
+      # event.message['text']でLINEで送られてきた文書を取得
+        if event.message['text'].include?("好き")
+          response = "んほぉぉぉぉぉぉ！すきすきすきすきすきすきすきすきぃぃぃぃぃ"
+        elsif event.message["text"].include?("行ってきます")
+          response = "どこいくの？どこいくの？どこいくの？寂しい寂しい寂しい。。。"
+        elsif event.message['text'].include?("おはよう")
+          response = "おはよう。なんで今まで連絡くれなかったの？"
+        elsif event.message['text'].include?("みーくん")
+          response = "みーくん！？" * 50
+        else
+          response = @post.name
+        end
+        #if文でresponseに送るメッセージを格納
 
       case event
       when Line::Bot::Event::Message
